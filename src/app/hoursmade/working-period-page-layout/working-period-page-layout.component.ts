6bb9e7c6dbed43b4eeb72f26c425c24470446ab1
Ids <idsmesales@gmail.com>
2018-07-21 20:02:59 +0200

    BASIC_IMPL>Need Refactoring but works.

--- src/app/hoursmade/working-period-page-layout/working-period-page-layout.component.ts
+++ src/app/hoursmade/working-period-page-layout/working-period-page-layout.component.ts
@@ -1,15 +1,132 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ɵNOT_FOUND_CHECK_ONLY_ELEMENT_INJECTOR} from '@angular/core';
+import * as moment from 'moment';
+import 'moment/locale/nl';
+import {environment} from '../../../environments/environment';
+import {WorkedPeriod} from '../worked-period';
+import {HoursMadeHelper} from '../hours-made-helper';
 
 @Component({
   selector: 'ids-working-period-page-layout',
   templateUrl: './working-period-page-layout.component.html',
   styleUrls: ['./working-period-page-layout.component.css']
 })
+
+
 export class WorkingPeriodPageLayoutComponent implements OnInit {
+
+  USER_SETTINGS = environment.user;
+
+  errorMessage: string;
+  currentDate;
+  periods: WorkedPeriod[];
+  totalHours = 0;
+  currentMonth: number;
+  projectFound;
+  earningsThisMonth: number;
 
   constructor() { }
 
   ngOnInit() {
+    // Get current Time.
+    this.currentDate = moment();
+    console.log(this.currentDate.format('LLLL'));
+
+    console.log(`Days in Month:`, this.currentDate.daysInMonth());
+    console.log(`Days of Month:`, this.currentDate.date());
+    console.log(`Days of week:`, this.currentDate.day());
+
+
+    // Set local
+    console.log(moment.locale()); // en
+    moment.locale('nl');
+
+    // Create array of certain size currentMonth.
+    this.periods = new Array<WorkedPeriod>(moment().daysInMonth());
+
+    // Get project from config via code.
+    const projectCodeDefault = environment.user.settings.project_name_default;
+    const projectsFound = environment.user.projects.filter((data) => {
+      return data.name === projectCodeDefault ? true : false;
+    });
+
+
+    // Check uniqueness.
+    if (projectsFound.length === 1 ) {
+      // Found one happy scenario.
+      this.projectFound = projectsFound[0];
+    } else if ( projectsFound.length === 0) {
+      console.error('More then one projects has the same code or none where found.>', projectsFound);
+      if (environment.user.projects.length === 0) {
+        console.error('You have to create a project first');
+        this.errorMessage = 'App contains 0 projects';
+      } else { // There are some projects but default one not is not set, take first one.
+        this.projectFound = environment.user.projects[0];
+        // Correct and make first one found the default one.
+        environment.user.settings.project_name_default = this.projectFound.name;
+      }
+    }
+
+    const beginOfMonthDate = moment();
+    beginOfMonthDate.date(1);
+    beginOfMonthDate.hour(0);
+    beginOfMonthDate.second(0);
+    beginOfMonthDate.minute(0);
+    console.log(`beginOfMonthDate>`, beginOfMonthDate.format('LLLL'));
+    console.log(`weekday>`, beginOfMonthDate.weekday()); // 6 == zondag.
+
+
+    // TODO IDSME set settings from correct project in newly generated WorkedPeriod Objects
+    for (let n = 0; n < this.periods.length; n++) {
+      // TODO IDSME Date of 0 Day of Month
+      const dayOfWeek = (n + beginOfMonthDate.weekday()) % 7;
+      console.log(`Mod 7 of ${dayOfWeek} `, environment.user.settings.work_days[dayOfWeek]);
+
+      if (environment.user.settings.work_days[dayOfWeek]) {
+
+        // TODO IDSME Add a constructor to add just two objects.
+        this.periods[n] = new WorkedPeriod(this.projectFound.name, this.projectFound.clientCode, this.projectFound.hoursWorked, this.projectFound.rate, n,
+          this.currentDate.month, this.currentDate.year);
+      } else { // It is weekend on none working day.
+        this.periods[n] = new WorkedPeriod(this.projectFound.name, this.projectFound.clientCode, this.projectFound.hoursWorked, this.projectFound.rate, n,
+          this.currentDate.month, this.currentDate.year);
+          HoursMadeHelper.convertDefaultFreePeriod(this.periods[n]);
+      }
+    }
+
+    this.calculate();
+
+  }
+
+  calculate() {
+    this.totalHours = -13;
+    let totalHoursNew = 0;
+    this.periods.forEach((currentWorkingPeriod) => {
+      totalHoursNew += currentWorkingPeriod.hoursWorked;
+      this.totalHours = totalHoursNew;
+    });
+    this.earningsThisMonth = this.projectFound.rate * this.totalHours;
+  }
+
+  clear(dayOfTheMonth: number) {
+    console.log(`clear>DayOfTheMonth`, dayOfTheMonth);
+
+    HoursMadeHelper.convertDefaultFreePeriod(this.periods[dayOfTheMonth]);
+    this.calculate();
+  }
+
+  fill(dayOfTheMonth: number) {
+    console.log(`reinit>DayOfTheMonth`, dayOfTheMonth);
+    console.log(`Fill with>`, this.projectFound);
+
+    this.periods[dayOfTheMonth] = new WorkedPeriod(this.projectFound.name, this.projectFound.clientCode, this.projectFound.hoursWorked, this.projectFound.rateAtThatTime, dayOfTheMonth,
+      this.currentDate.month, this.currentDate.year);
+    this.calculate();
+  }
+
+  plus(plus: number, currentPeriod: WorkedPeriod) {
+    console.log(`plus>${plus}>`, currentPeriod);
+    currentPeriod.hoursWorked += plus;
+    this.calculate();
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
+// This fill can be replaced during build by using the `fileReplacements` array.
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// The list of fill replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
@@ -9,8 +9,21 @@ export const environment = {
       name: 'I. Achterhof',
       hours_per_day: 8,
       work_days: [1, 1, 0, 1, 1, 0, 0],
-      rate_per_hour: 90
-    }
+      rate_per_hour: 90,
+      project_name_default: '999',
+    },
+    projects : [
+      {
+        name: 'project x',
+        clientCode: '999',
+        rate: 90,
+      },
+      {
+        name: 'project b',
+        clientCode: '667',
+        rate: 85,
+      }
+    ]
 
   }
 };
@@ -18,7 +31,7 @@ export const environment = {
 /*
  * In development mode, to ignore zone related error stack frames such as
  * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
+ * import the following fill, but please comment it out in production mode
  * because it will have performance impact when throw error
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
