9af629c290d9d293aa415fd7b7084d136f6dc50a
Ids <idsmesales@gmail.com>
2018-08-11 01:11:24 +0200

    Refacttor>Moved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ids-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Timeout the ultimate monthly hours overview app';
