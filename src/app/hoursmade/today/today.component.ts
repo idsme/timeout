8da72269759f020b201c944f47074969f7b35363
Ids <idsmesales@gmail.com>
2020-08-06 01:12:24 +0200

    Added more to for debugging it later.

--- src/app/hoursmade/today/today.component.ts
+++ src/app/hoursmade/today/today.component.ts
@@ -8,12 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class TodayComponent implements OnInit {
   millisecondsSindsStart = 131313;
   startDateTime = new Date();
+  currentDateTime = new Date();
 
   constructor() { }
 
   ngOnInit() {
     this.startDateTime.setHours(9, 0, 0);
     this.millisecondsSindsStart = new Date().getTime() - this.startDateTime.getTime();
+    if (this.millisecondsSindsStart < 0) {
+       this.millisecondsSindsStart = this.millisecondsSindsStart * -1;
+    }
   }
 
 
