463bedd0d9c7b6e60754262d7c919ebf1b5de889
achterhofi <ids.achterhof@tno.nl>
2020-09-06 13:56:31 +0200

    Make vuetify work manual configged.add_vuetify debugged it with help from forms and google. node-sass did not work with vuetify, reworked it with sass.

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import 'vuetify/dist/vuetify.min.css'
+import vuetify from "vuetify/lib";
+
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   store,
   render: h => h(App)
 }).$mount("#app");
